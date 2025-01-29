2239b2cd7e259fc26c960815157b9d9b130eae7c
Vatsal Sanjay <vatsalsanjay@gmail.com>
2025-01-29 16:33:43 +0100

    added another ignore list

--- Sources/ExportOtherRemindersCLI/main.swift
+++ Sources/ExportOtherRemindersCLI/main.swift
@@ -13,6 +13,7 @@ struct ExportOtherRemindersCLI {
             "Future-shopping-list",
             "Books-to-listen-Anjali",
             "Anjali-internship-tasks",
+            "Slack-ReadOnly",
             "obsidian"  // Always exclude obsidian list
         ]
         // Add vault name to excluded lists
